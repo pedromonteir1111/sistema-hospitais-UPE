55ebcd8a3535805ad5d79fc2c478f78608439f58
jonatasalex <143233123+jonatasalex@users.noreply.github.com>
2024-03-06 22:20:42 -0300

    Lógica de logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginBox from './components/LoginBox';
 import RegisterBox from './components/RegisterBox';
+import UserBox from './components/UserBox';
 import FazerUpload from './components/UploadBox';
-import axios from 'axios';
+
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginBox />} />
         <Route path="/register" element={<RegisterBox />} />
+        <Route path="/user" element={<UserBox />} />
         <Route path="/upload" element={<FazerUpload />} />
         {/* <Route path="/home" element={<HomeBox />} /> */}
       </Routes>
