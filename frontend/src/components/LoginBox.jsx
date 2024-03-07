6720de881aec101b5b7a35fe1336b516f4546155
rafaelmslima <138700330+rafaelmslima@users.noreply.github.com>
2024-03-07 18:48:58 -0300

    Corrigindo botão de cadastro

--- frontend/src/components/LoginBox.jsx
+++ frontend/src/components/LoginBox.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import logo from "./logo.png"
+import logo from "./upe.jpg"
 import { Link, useNavigate } from 'react-router-dom';
 import './LoginBox.css'
 import axios from 'axios';
@@ -39,33 +39,35 @@ function LoginBox() {
 
   Checar_cadastro();
   return (
-    <div className="login-form-wrap">
-      <div className="container">
+    <div className='fundo-login'>
+      <div className="login-form-wrap">
+        <div className="container">
         <img src={logo} alt="Logo"/>
-      </div>
-      <h2 className = 'texto-login'>Acessar Sistema</h2>
-      <div>     
-        <form className = 'login-form' onSubmit={enviarDadosParaBackend}>
-        <input name="username" placeholder="Nome" required
-        onChange={(e) => setLogin(e.target.value)}
-        value={login} 
-        />
-        <input type="password" name="password" placeholder="Senha" 
-        onChange={(e) => setSenha(e.target.value)}
-        value={senha}
-        />
+        </div>
+        <h2 className = 'texto-login'>Acessar Sistema</h2>
+        <div>     
+          <form className = 'login-form' onSubmit={enviarDadosParaBackend}>
+          <input name="username" placeholder="Nome" required
+          onChange={(e) => setLogin(e.target.value)}
+          value={login} 
+          />
+          <input type="password" name="password" placeholder="Senha" 
+          onChange={(e) => setSenha(e.target.value)}
+          value={senha}
+          />
 
-        <button type="submit" className="btn-login">
-          Entrar
-        </button>
-        </form>
-        {respostaDoBackend && (
-          <p>{respostaDoBackend.mensagem}</p>
-        )}
+          <button type="submit" className="btn-login">
+            Entrar
+          </button>
+          </form>
+          {respostaDoBackend && (
+            <p>{respostaDoBackend.mensagem}</p>
+          )}
 
-        <Link to="/register">
-          <button className="btn-login">Criar conta</button>
-        </Link>
+          <Link to="/register">
+            <button className="btn-register">Criar conta</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
