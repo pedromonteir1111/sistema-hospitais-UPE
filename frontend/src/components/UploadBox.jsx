9c1a1bc4469ebae5a180b0b14d8aa8d9dbbaadea
manodaros <142640900+manodaros@users.noreply.github.com>
2024-03-20 21:23:37 -0300

    Versão correta de UploadBox.jsx
    FALTA O FRONT! (css e html)

--- frontend/src/components/UploadBox.jsx
+++ frontend/src/components/UploadBox.jsx
@@ -6,13 +6,15 @@ function FazerUpload() {
     const [arquivoUsuario, setFile] = useState();
 
     function selecionarArquivo_FrontEnd(event) {
+        console.log('Arquivo selecionado:', event.target.files[0]);
         setFile(event.target.files[0]);
     }
 
     async function Input_arquivoUsuario(event) {
         event.preventDefault();
-        const url = 'http://localhost:4000/api/enviar-dados/upload';
+        const url = 'http://localhost:4000/api/enviar-dados/uploader';
         const formData = new FormData();
+       
         formData.append('file', arquivoUsuario);
         formData.append('fileName', arquivoUsuario.name);
         const config = {
@@ -21,23 +23,22 @@ function FazerUpload() {
             },
         };
         try {
-            const respostaFoienviado = await axios.post(url, formData, config);
-            console.log(respostaFoienviado.data);
+            var resposta_Foienviado = axios.post(url, formData, config);
             console.log("Arquivo enviado com sucesso!!!")
-            // Aqui você pode tratar a resposta, se necessário
+           
+        
         } catch (error) {
             console.error('Erro ao enviar arquivo:', error);
-            console.log("Erro aoo enviar")
-            // Aqui você pode tratar o erro, se necessário
+            
         }
     }
 
     return (
         <div className="App">
             <form onSubmit={Input_arquivoUsuario}>
-                <h1>React File Upload</h1>
+                <h1>ENVIO DE RESULTADOS</h1>
                 <input type="file" onChange={selecionarArquivo_FrontEnd} />
-                <button type="submit">Upload</button>
+                <button type="submit">ENVIAR</button>
             </form>
         </div>
     );
@@ -45,65 +46,6 @@ function FazerUpload() {
 
 export default FazerUpload;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import './UploadBox.css';
-
-// function FazerUpload(){
-
-// const [arquivoUsuario, setFile] = useState()  //arquivoUsuario armazenará o Arquivo
-
-// function selecionarArquivo_FrontEnd(event) {  //funçao Atualiza a var arquivoUsuario com o arquivo selecionado no FrontEnd
-//     setFile(event.target.files[0])       
-// }
-
-// function Input_arquivoUsuario(event) {
-//     event.preventDefault()
-//     const url = 'http://localhost:3000/uploadFile';
-//     const formData = new FormData();
-//     formData.append('file', arquivoUsuario);
-//     formData.append('fileName', arquivoUsuario.name);
-//     const config = {
-//     headers: {
-//         'content-type': 'multipart/form-data',
-//     },
-//     };
-//     axios.post(url, formData, config).then((response) => {
-//     console.log(response.data);
-//     });
-
-// }
-
-// return (
-//     <div className="App">
-//         <form onSubmit={Input_arquivoUsuario}>
-//         <h1>React File Upload</h1>
-//         <input type="file" onChange={selecionarArquivo_FrontEnd}/>
-//         <button type="submit">Upload</button>
-//         </form>
-//     </div>
-// );
-// }
-
-// export default FazerUpload;
 
 
 
