5ecade2158b0aafac3a8084d349b49b981247e00
jonatasalex <143233123+jonatasalex@users.noreply.github.com>
2024-03-21 00:14:23 -0300

    Atualização de vínculo do médico

--- backend/routers.js
+++ backend/routers.js
@@ -6,9 +6,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const { log } = require('console');
 const { PassThrough } = require('stream');
-const port = 4000; 
-const multer = require('multer') // Middleware que ajuda no Upload
 const path = require('path')
+const multer = require('multer')
+const port = 4000; 
 
 var nome;
 var nome_inicial;
@@ -17,12 +17,17 @@ var UsuarioExistente = false;
 var resposta_de_login;
 var resposta_de_registro;
 var resposta_de_user;
+var random_id;
+var lista_ids = [];
+var lista_ids_dos_pacientes = []
+var medico_que_esta_logado;
+
 
 app.use(cors()); // para permitir solicitações de qualquer origem
 app.use(bodyParser.json());
 
 app.post('/api/enviar-dados/registrar', (req, res) => {
-    const novoUsuario = req.body;
+    var novoUsuario = req.body;
     nome = ""
     console.log('Dados recebidos do frontend:', novoUsuario);
 
@@ -50,6 +55,19 @@ app.post('/api/enviar-dados/registrar', (req, res) => {
           } 
           
           else {
+            lista_ids = []
+            random_id = Math.floor(Math.random() * 100000000);
+            console.log(random_id)
+            for(let paciente in Dados_antigos.usuarios){
+              lista_ids.push(Dados_antigos.usuarios[paciente].p_id)
+              if(Dados_antigos.usuarios[paciente].p_id == random_id || lista_ids.includes(random_id)){
+                random_id = Math.floor(Math.random() * 100000000);
+              }
+
+            }
+            console.log("usuário antes:", novoUsuario)
+            novoUsuario.p_id = random_id
+            console.log(novoUsuario)
             Dados_antigos.usuarios.push(novoUsuario);
             resposta_de_registro = { mensagem: 'Dados recebidos com sucesso!' };
             res.json(resposta_de_registro);
@@ -86,6 +104,11 @@ app.post('/api/enviar-dados/logar', (req, res) => {
           UsuarioExistente = true;
           nome = Dados_antigos.usuarios[indice_dos_dados].nome;
           medico = Dados_antigos.usuarios[indice_dos_dados].medico;
+          if(medico == true){
+            lista_ids_dos_pacientes = Dados_antigos.usuarios[indice_dos_dados].pacientes
+            medico_que_esta_logado = Dados_antigos.usuarios[indice_dos_dados].id
+
+          }
           console.log(Dados_antigos.usuarios[indice_dos_dados])
           console.log(nome)
           try{
@@ -125,7 +148,7 @@ app.post('/api/enviar-dados/user', (req, res) => {
   if(resposta_de_login.mensagem == 'Login realizado'){
     
     if(requisicao.mensagem == 'Quero saber o usuário'){
-      resposta_de_user = {nome: nome_inicial, isMedico: medico, mensagem: 'Login feito' };
+      resposta_de_user = {nome: nome_inicial, isMedico: medico, mensagem: 'Login feito'};
       console.log(`O usuário ${nome} fez login`);
       res.json(resposta_de_user);
     }
@@ -137,32 +160,51 @@ app.post('/api/enviar-dados/user', (req, res) => {
 
 })
 
+app.post('/api/enviar-dados/logout', (req, res) => {
+  const requisicao = req.body;
+  console.log('Requisição recebida em /api/enviar-dados/logout:', req.body)
+  if(requisicao.mensagem === 'Quero sair'){
+    res.json({mensagem: 'Saia'});
+    console.log('O usuário pediu pra sair')
+    // nome = ''
+    resposta_de_login.mensagem = ''
+  }
+  else{
+    res.json({mensagem: 'Não saia'});
+  }
+
+
+})
+
 const upload = multer({ dest: 'Arquivos_Enviados_Upload/' });
 
 app.post('/api/enviar-dados/uploader', upload.single('file'), (req, res) => {
-    const ID_arquivo = req.file; // Aqui está o arquivo enviado
-    const Nome_arquivo = req.body.fileName; // Caso o arquivo esteja no CORPO de solicitação
-    
-    // verificar se o arquivo foi recebido
-    if (!ID_arquivo || !Nome_arquivo) {
-        console.log('Dados Não recebidos');
-        return res.status(400).send('Dados do arquivo ausentes ou inválidos');
-    }
+  const ID_arquivo = req.file; // Aqui está o arquivo enviado
+  const Nome_arquivo = req.body.fileName; // Caso o arquivo esteja no CORPO de solicitação
 
-    // mover o arquivo para a pasta Arquivos_Enviados_Upload
-    const Path_arquivo = path.join(__dirname, 'Arquivos_Enviados_Upload', Nome_arquivo);
+  // verificar se o arquivo foi recebido
+  if (!ID_arquivo || !Nome_arquivo) {
+      console.log('Dados Não recebidos');
+      return res.status(400).send('Dados do arquivo ausentes ou inválidos');
+  }
 
-    fs.rename(ID_arquivo.path, Path_arquivo, (err) => {
-        if (err) {
-            console.log("Erro ao salvar arquivo:", err);
-            return res.status(500).send('Erro ao salvar o arquivo');
-        }
-        console.log("Arquivo salvo com sucesso em:", Path_arquivo);
-        res.send('Arquivo enviado e salvo com sucesso');
-    });
+  // mover o arquivo para a pasta Arquivos_Enviados_Upload
+  const Path_arquivo = path.join(__dirname, 'Arquivos_Enviados_Upload', Nome_arquivo);
+
+  fs.rename(ID_arquivo.path, Path_arquivo, (err) => {
+      if (err) {
+          console.log("Erro ao salvar arquivo:", err);
+          return res.status(500).send('Erro ao salvar o arquivo');
+      }
+      console.log("Arquivo salvo com sucesso em:", Path_arquivo);
+      res.send('Arquivo enviado e salvo com sucesso');
+  });
 });
 
-
+  app.listen(port, () => {
+    console.log(`Servidor backend está rodando em http://localhost:${port}`);
+  });
+  
   app.post('/api/enviar-dados/user/medico/exames', (req, res) => {
     const requisicao = req.body;
        
@@ -180,7 +222,7 @@ app.post('/api/enviar-dados/uploader', upload.single('file'), (req, res) => {
   })
 
   app.post('/api/enviar-dados/user/medico/pacientes', (req, res) => {
-    const requisicao = req.body;
+    var requisicao = req.body;
        
     if(requisicao.mensagem === 'Quero os pacientes'){
       fs.readFile(bancodedados, 'utf8', (err, datastr) => {
@@ -189,13 +231,51 @@ app.post('/api/enviar-dados/uploader', upload.single('file'), (req, res) => {
           return;
         }
         const data = JSON.parse(datastr);
-        const pacientes = data.usuarios.filter(usuario => !usuario.medico && usuario.nome)
+        const pacientes = data.usuarios.filter(usuario => !usuario.medico && usuario.nome && lista_ids_dos_pacientes.includes(usuario.p_id))
         console.log(pacientes.map((paciente) => {return {name: paciente.nome, text: paciente.cpf}}));
         res.json(pacientes.map((paciente) => {return {name: paciente.nome, text: paciente.cpf,}}));
       })
     }
   })
 
-  app.listen(port, () => {
-    console.log(`Servidor backend está rodando em http://localhost:${port}`);
+  app.post('/api/enviar-dados/user/medico/pacientes/vincular', (req, res) => {
+    var requisicao = req.body;
+  
+    if (requisicao.mensagem === 'Quero vincular um paciente') {
+      fs.readFile(bancodedados, 'utf8', (err, datastr) => {
+        if (err) {
+          console.error('Erro ao ler o banco de dados', err);
+          res.status(500).json({ mensagem: 'Erro ao ler o banco de dados' });
+          return;
+        }
+  
+        var data = JSON.parse(datastr);
+        for (let user in data.usuarios) {
+          if (data.usuarios[user].id == medico_que_esta_logado) {
+            for (let i in data.usuarios) {
+              if (data.usuarios[i].cpf == requisicao.cpf) {
+                data.usuarios[user].pacientes.push(data.usuarios[i].p_id);
+                console.log(data.usuarios[i].p_id)
+              }
+            }
+          }
+        }
+        
+        const banco_atualizado = JSON.stringify(data, null, 2);
+
+        fs.writeFile(bancodedados, banco_atualizado, 'utf8', (err) => {
+          if (err) {
+            console.error('Erro ao escrever no banco de dados', err);
+            res.status(500).json({ mensagem: 'Erro ao escrever no banco de dados' });
+            return;
+          }
+          console.log('Dados adicionados ao arquivo.');
+          res.json({ mensagem: 'Vinculei' });
+        });
+      });
+    }
   });
+
+
+  // const pacientes = data.usuarios.filter(usuario => !usuario.medico && usuario.nome && lista_ids_dos_pacientes.includes(usuario.p_id))
+        // console.log(pacientes.map((paciente) => {return {name: paciente.nome, text: paciente.cpf}}));
